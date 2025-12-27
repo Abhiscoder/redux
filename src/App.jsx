14bb8a1a9f1a1bf5359ff4d5fc2bf11d0015d036
Abhiscoder <abhishekkumar200m1@gmail.com>
2025-12-27 12:04:02 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-
-import { useDispatch, useSelector } from "react-redux"
-import { decrement, increment, incrementByAmount } from "./features/counterSlice"
+import React from 'react'
 
 const App = () => {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
   return (
-    <div>
-      <h1>value: {count}</h1>
-      <button onClick={
-        () => { dispatch(increment()) }
-      }>increment</button>
-      <button onClick={
-        () => { dispatch(decrement()) }
-      }>decrement</button>
-      <button onClick={
-        () => { dispatch(incrementByAmount(50)) }
-      }>incrementByAmount</button>
-    </div >
+    <div>App</div>
   )
 }
 
