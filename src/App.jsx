45f23d3c730752a6da169105ac4619bad0003c96
Abhiscoder <abhishekkumar200m1@gmail.com>
2025-12-31 14:27:15 +0530

    How to fetch data from API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { fetchPhotos } from "./api/mediaApi"
 
 const App = () => {
+
   return (
     <div className="h-screen text-white w-full bg-gray-950">
-      
-    </div>
+      <button onClick={async () => {
+        const data = await fetchPhotos('dog')
+        console.log("Data:", data)
+        console.log("Data_results:", data.results)
+      }}>GEt Photo</button>
+    </div >
   )
 }
 
